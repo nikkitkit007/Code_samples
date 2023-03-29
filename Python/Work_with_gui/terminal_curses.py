ded3b3406301395bb6389605db506e7f83a235e2
Ilya-Kovalenko <98124003+Ilya-Kovalenko@users.noreply.github.com>
2023-03-29 23:16:48 +0300

    Move terminal_curses.py to Work_with_gui directory. Add logo to terminal_curses and info about program

--- Python/Work_with_gui/terminal_curses.py
+++ Python/Work_with_gui/terminal_curses.py
@@ -1,7 +1,9 @@
 import curses
 import os
-from curses.textpad import Textbox, rectangle
+import time
+from curses.textpad import rectangle
 from curses import wrapper
+from logo_curses import show_logo
 
 
 def update_view_win(view_win, dir_to_view):
@@ -23,8 +25,6 @@ def sort_dir_files(unsorted_dir):
         else:
             dir_list.append(el)
 
-    files_list.remove("terminal_curses.py")
-
     files_list.sort()
     dir_list.sort()
 
@@ -43,8 +43,12 @@ def sort_dir_files(unsorted_dir):
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)  # инициализация цветов
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
     YELLOW = curses.color_pair(1)
     GREEN = curses.color_pair(2)
+    CYAN = curses.color_pair(3)
+
+    curses.curs_set(0)
 
     cur_position = 0
 
@@ -53,11 +57,17 @@ def main(stdscr):
     navi_win = curses.newwin(20, 32, 2, 1)  # создание окон
     view_win = curses.newwin(20, 40, 2, 36)
 
+    show_logo(stdscr, CYAN)
+    time.sleep(1.7)
+
     stdscr.clear()
 
     stdscr.addstr(0, 1, "Code Samples", curses.A_BOLD)
     rectangle(stdscr, 1, 0, 1 + 20 + 1, 1 + 32 + 1)  # отрисовка границ вокруг окон
     rectangle(stdscr, 1, 35, 1 + 20 + 1, 1 + 74 + 1)
+    rectangle(stdscr, 23, 0, 26, 76)  # отрисовка нижнего бара
+    stdscr.addstr(24, 1, "Use arrows to navigate", CYAN | curses.A_BOLD)
+    stdscr.addstr(25, 1, "Press q to exit the program", CYAN | curses.A_BOLD)
 
     stdscr.refresh()
 
@@ -81,17 +91,23 @@ def main(stdscr):
         navi_win.refresh()
 
         key = stdscr.getkey()
-        if key == "KEY_UP":
+        if key == "KEY_UP":  # обработка нажатий клавиш
             if cur_position > 0:
                 dir_files[cur_position]['is_current'] = False
                 cur_position -= 1
                 dir_files[cur_position]['is_current'] = True
+            else:
+                curses.beep()
+                curses.flash()
 
         elif key == "KEY_DOWN":
             if cur_position < len(dir_files)-1:
                 dir_files[cur_position]['is_current'] = False
                 cur_position += 1
                 dir_files[cur_position]['is_current'] = True
+            else:
+                curses.beep()
+                curses.flash()
 
         elif key == "q" or key == "Q":
             break
@@ -100,5 +116,4 @@ def main(stdscr):
 if __name__ == "__main__":
     wrapper(main)
 
-    # todo сделать заставку
     # todo улучшить функциональность обозревателя папок
